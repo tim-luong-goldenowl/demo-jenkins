84a88849587f22205becdb86a898219df6c52973
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2023-05-21 16:22:52 +0700

    aaa

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
-output "jenkins_terraform" {
-  value = "running Terraform from Jenkin"
+resource "aws_instance" "web" {
+  ami           = "ami-0406b8387ac37a82a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tim-first-instance"
+  }
 }
