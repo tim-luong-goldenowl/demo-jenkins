5205443cc6ce9661f78ba9b0e6db6ff10b9ae9c5
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2023-05-23 22:04:53 +0700

    eee

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
-output "jenkins_terraform" {
-  value = "running Terraform from Jenkin"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0406b8387ac37a82a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tim-first-instance"
+  }
 }
--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0406b8387ac37a82a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "tim-first-instance"
-  }
-}
