5d2e0883a68604d6e89057366676f171c03986ab
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2023-05-24 20:52:39 +0700

    aaa

--- main.tf
+++ main.tf
@@ -3,73 +3,77 @@ variable "IMAGE_NAME" {
   default = "asdad"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
+output "jenkins_terraform" {
+  value = var.IMAGE_NAME
 }
 
-provider "aws" {
-  region = "ap-southeast-1"
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
 
-resource "aws_security_group" "ec2_sg" {
-  name        = "allow_http"
-  description = "Allow http inbound traffic"
+# provider "aws" {
+#   region = "ap-southeast-1"
+# }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# resource "aws_security_group" "ec2_sg" {
+#   name        = "allow_http"
+#   description = "Allow http inbound traffic"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "terraform-ec2-security-group"
-  }
-}
+#   egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     cidr_blocks     = ["0.0.0.0/0"]
+#   }
 
-resource "aws_instance" "web" {
-  ami           = "ami-054c486632a4875d3"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  user_data = <<EOF
-    #!/bin/bash
+#   tags = {
+#     Name = "terraform-ec2-security-group"
+#   }
+# }
 
-    sudo yum update -y
-    sudo yum install docker -y
-    sudo usermod -a -G docker ec2-user
-    newgrp docker
-    sudo systemctl enable docker.service
-    sudo systemctl start docker.service
-    sudo docker run -p 80:3000 -d ${var.IMAGE_NAME}
-  EOF
+# resource "aws_instance" "web" {
+#   ami           = "ami-054c486632a4875d3"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+#   user_data = <<EOF
+#     #!/bin/bash
 
-  tags = {
-    Name = "tim-first-instance"
-  }
-}
+#     sudo yum update -y
+#     sudo yum install docker -y
+#     sudo usermod -a -G docker ec2-user
+#     newgrp docker
+#     sudo systemctl enable docker.service
+#     sudo systemctl start docker.service
+#     sudo docker run -p 80:3000 -d ${var.IMAGE_NAME}
+#   EOF
+
+#   tags = {
+#     Name = "tim-first-instance"
+#   }
+# }
