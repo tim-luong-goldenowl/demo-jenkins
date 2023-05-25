667cc304aacac06b5d450405d738353d387e29d0
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2023-05-25 17:27:14 +0700

    second branch commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Worldddd master ok!')
+  res.send('Hello from second-brach!')
 })
 
 app.listen(port, () => {
