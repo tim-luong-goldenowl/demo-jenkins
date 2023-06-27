bb079ac908644449449b4c591128b2d056a6e936
tim-luong-goldenowl <tim.luong.goldenowl@gmail.com>
2023-06-27 12:18:25 +0700

    add env

--- queries.js
+++ queries.js
@@ -1,31 +1,32 @@
+require('dotenv').config();
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'tim_demo',
-  password: '123456',
+  user: process.env.USER_NAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: 5432,
 })
 
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.render("userList", {userList:results.rows});
-    //   response.status(200).json(results.rows)
-    })
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.render("userList", {userList:results.rows});
+  })
 }
 
 const createUser = (request, response) => {
-    const { name, email } = request.body
-  
-    pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`User added with ID: ${results.rows[0].id}`)
-    })
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+  })
 }
 
 module.exports = {
